334a134feefc1693c1ddb05888873db49fc3e03c
KanchanJadhav13 <kanchanwavhale2013@gmail.com>
2024-01-14 22:13:29 +0530

    [Kanchan]:adding new class

---
+++ src/abstractDemo/KanchanProject.java
@@ -0,0 +1,11 @@
+package abstractDemo;
+
+public class KanchanProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hii this is kanchan");
+
+	}
+
+}
